79344a52f19170f996632f9d3cb671f838d8b33b
lahsen rabid <115492656+lahsen1abid@users.noreply.github.com>
2022-10-16 12:56:56 +0200

    Add files via upload

---
+++ calculer.sh
@@ -0,0 +1,14 @@
+name="$0"
+name1="author = lahsen rabid"
+echo "$name1"
+echo "--- github : "
+echo "--- file name : $0"
+read -p  "\____ Calculer : number1= " num1 
+read -p "\____ Calculer : +-x/= " num
+read -p "\____ Calculer : number2= " num2
+echo "\____ la reponse :  $(($num1 $num $num2))"
+echo    "___________________"
+echo    "I                 I"
+echo    "I ctrl + C = exit I"
+echo    "I_________________I" 
+bash calculer.sh 
